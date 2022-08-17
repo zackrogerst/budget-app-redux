2d972fdefb1eee073787085c942d22662ef5a956
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-17 15:42:44 -0600

    updated Budget component and budgetReducer.js

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -9,14 +9,20 @@ import Nav from "./../shared/Nav";
 import "./Budget.css";
 import { connect } from "react-redux";
 import { requestUserData } from "./../../redux/userReducer";
+import {
+	requestBudgetData,
+	addPurchase,
+	removePurchase
+} from "./../../redux/budgetReducer";
 
 class Budget extends Component {
 	componentDidMount() {
 		this.props.requestUserData();
+		this.props.requestBudgetData();
 	}
 
 	render() {
-		const { loading } = this.props.budget;
+		const { loading, purchases, budgetLimit } = this.props.budget;
 		const { firstName, lastName } = this.props.user;
 		return (
 			<Background>
@@ -25,12 +31,18 @@ class Budget extends Component {
 					<Nav firstName={firstName} lastName={lastName} />
 					<div className="content-container">
 						<div className="purchases-container">
-							<AddPurchase />
-							<DisplayPurchases />
+							<AddPurchase addPurchase={this.props.addPurchase} />
+							<DisplayPurchases
+								purchases={purchases}
+								removePurchase={this.props.removePurchase}
+							/>
 						</div>
 						<div className="chart-container">
-							<Chart1 />
-							<Chart2 />
+							<Chart1
+								purchases={purchases}
+								budgetLimit={budgetLimit}
+							/>
+							<Chart2 purchases={purchases} />
 						</div>
 					</div>
 				</div>
@@ -46,4 +58,9 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps, { requestUserData })(Budget);
+export default connect(mapStateToProps, {
+	requestUserData,
+	requestBudgetData,
+	addPurchase,
+	removePurchase
+})(Budget);

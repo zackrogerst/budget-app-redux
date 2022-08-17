3d6966a6e9ab63c7759130015bf18798c9f8d5a9
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-17 12:34:01 -0600

    added budget state to Budget.js, managed loading state

--- src/Components/Budget/Budget.js
+++ src/Components/Budget/Budget.js
@@ -1,36 +1,42 @@
-import React, { Component } from 'react';
-import Background from './../shared/Background/Background'
-import Chart1 from './../shared/Chart1';
-import Chart2 from './../shared/Chart2';
-import AddPurchase from './../shared/AddPurchase';
-import DisplayPurchases from './../shared/DisplayPurchases';
-import Loading from './../shared/Loading/Loading';
-import Nav from './../shared/Nav';
-import './Budget.css';
-
+import React, { Component } from "react";
+import Background from "./../shared/Background/Background";
+import Chart1 from "./../shared/Chart1";
+import Chart2 from "./../shared/Chart2";
+import AddPurchase from "./../shared/AddPurchase";
+import DisplayPurchases from "./../shared/DisplayPurchases";
+import Loading from "./../shared/Loading/Loading";
+import Nav from "./../shared/Nav";
+import "./Budget.css";
+import { connect } from "react-redux";
 
 class Budget extends Component {
-
-  render() {
-    return (
-      <Background>
-        {true ? <Loading /> : null}
-        <div className='budget-container'>
-          <Nav />
-          <div className='content-container'>
-            <div className="purchases-container">
-              <AddPurchase />
-              <DisplayPurchases />
-            </div>
-            <div className='chart-container'>
-              <Chart1 />
-              <Chart2 />
-            </div>
-          </div>
-        </div>
-      </Background>
-    )
-  }
+	render() {
+		const { loading } = this.props.budget;
+		return (
+			<Background>
+				{loading ? <Loading /> : null}
+				<div className="budget-container">
+					<Nav />
+					<div className="content-container">
+						<div className="purchases-container">
+							<AddPurchase />
+							<DisplayPurchases />
+						</div>
+						<div className="chart-container">
+							<Chart1 />
+							<Chart2 />
+						</div>
+					</div>
+				</div>
+			</Background>
+		);
+	}
 }
 
-export default Budget;
+function mapStateToProps(state) {
+	return {
+		budget: state.budget
+	};
+}
+
+export default connect(mapStateToProps)(Budget);

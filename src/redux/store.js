777334cabf04033294d874535f77d925f2530ae9
Zack Rogers <zack.rogers.t@gmail.com>
2022-08-17 14:11:09 -0600

    added userReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from "redux";
 import promiseMiddleware from "redux-promise-middleware";
 import budgetReducer from "./budgetReducer";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
-	budget: budgetReducer
+	budget: budgetReducer,
+	user: userReducer
 });
 
 export default createStore(rootReducer, applyMiddleware(promiseMiddleware));
---
+++ src/redux/userReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+	email: null,
+	firstName: null,
+	lastName: null
+};
+
+function userReducer(state = initialState, action) {
+	return state;
+}
+
+export default userReducer;
